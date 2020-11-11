870c33c7222b93c304d75262d27c89173b17f65a
Jakub Dipčár <jakub.dipcar@gmail.com>
2020-11-12 00:05:22 +0100

    tada

--- Open-Lab-04.07/StringTools.cs
+++ Open-Lab-04.07/StringTools.cs
@@ -6,7 +6,17 @@ namespace Open_Lab_04._07
     {
         public string Reverse(string original)
         {
-            throw new NotImplementedException();
+            if (original == "")
+            {
+                return "";
+            }
+            char[] vysledokchar = new char[original.Length];
+            for (int i = original.Length; i > 0; i--)
+            {
+                vysledokchar[i - 1] = original[original.Length - i];
+            }
+            string vysledok = new string(vysledokchar);
+            return vysledok;
         }
     }
 }
